38b8f47e080cf4f5bdccf7e18bb897b091d4d192
Darya Tarasova <kuroifreya@gmail.com>
2023-07-31 20:53:06 +0300

    JUnit platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,11 +1,14 @@
 package ru.netology.service;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService hackService = new CashbackHackService();
+
+    //Тесты на JUnit4
 
     @Test
     public void ShouldHackAmount1000() {
@@ -38,6 +41,7 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
+
     @Test
     public void ShouldHackAmount0() {
         int amount = 1000;
@@ -48,6 +52,7 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
+
     @Test
     public void ShouldHackAmount999() {
         int amount = 1001;
@@ -56,5 +61,60 @@ public class CashbackHackServiceTest {
         int expected = 999;
 
         assertEquals(expected, actual);
+    }
+
+    //Тесты на JUnit Jupiter
+
+    @Test
+    public void ShouldHackAmount1000Jupiter() {
+
+        int amount = 0;
+
+        int actual = hackService.remain(amount);
+        int expected = 1000;
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void ShouldHackAmount500Jupiter() {
+        int amount = 500;
+
+        int actual = hackService.remain(amount);
+        int expected = 500;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldHackAmount1Jupiter() {
+        int amount = 999;
+
+        int actual = hackService.remain(amount);
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void ShouldHackAmount0Jupiter() {
+        int amount = 1000;
+
+        int actual = hackService.remain(amount);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void ShouldHackAmount999Jupiter() {
+        int amount = 1001;
+
+        int actual = hackService.remain(amount);
+        int expected = 999;
+
+        Assertions.assertEquals(expected, actual);
     }
 }
